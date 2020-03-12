a46b11cf62677e2e039b3bf92ad2a60a9b219e55
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-12 05:24:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,7 @@
 def prime?(integer)
   return false if integer < 2
-  (2..(integer - 1)).none? {|i| integer % i == 0}
+  (2..(integer - 1)).each do |n|
+    return false if integer % n == 0
+  end
+  true
 end
